aeabe7cd75d539058a4dbaa68e6e26a738ed1584
university <universidad@gmail.com>
2025-09-24 18:56:50 -0600

    first commit

---
+++ src/app.module.ts
@@ -0,0 +1,23 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'password123*',
+      database: 'e_commercebd',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],//aqui vamos agregando las entidades que tengan .entity
+      synchronize: true,
+    }),
+    UsersModule,
+  ],
+  })
+
+export class AppModule {}
