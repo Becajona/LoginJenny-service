c3315cbb1924393390a638d80221db5991cffeab
university <universidad@gmail.com>
2025-09-25 17:50:58 -0600

    Funcioness

---
+++ src/auth/dto/register-auth.dto.ts
@@ -0,0 +1,30 @@
+import { 
+  IsEmail, 
+  IsNotEmpty, 
+  IsString, 
+  MinLength 
+} from 'class-validator';
+
+export class RegisterAuthDto {
+  @IsNotEmpty()
+  @IsString()
+  name: string;
+
+  @IsNotEmpty()
+  @IsString()
+  lastname: string;
+
+  @IsNotEmpty()
+  @IsString()
+  phone: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @IsEmail({}, { message: 'El email no es válido' })
+  email: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @MinLength(6, { message: 'Mínimo 6 caracteres' })
+  password: string;
+}
